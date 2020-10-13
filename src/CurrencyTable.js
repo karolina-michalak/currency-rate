268fea23b628d5d4062f87b8d19620cf62c8e4ad
Karolina Michalak <karolina0michalak@gmail.com>
2020-10-13 23:13:25 +0200

    displaying all the currencies

--- src/CurrencyTable.js
+++ src/CurrencyTable.js
@@ -1,65 +1,34 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
-// class CurrencyTable extends React.Component {
-//     constructor(props) {
-//       super(props);
-//       this.state = {
-//         error: null,
-//         isLoaded: false,
-//         items: []
-//       };
-//     }
-  
-//     componentDidMount() {
-//       fetch("http://api.nbp.pl/api/exchangerates/rates/A/CHF/?format=json")
-//         .then(res => res.json())
-//         .then(
-//           (result) => {
-//             console.log(result.rates[0].mid)
-//           },
+function CurrencyTable() {
+  const [items, setItems] = useState([]);
+  const [error, setError] = useState(null);
 
-        
-//         )
-//     }
-  
-//     render() {
-//       return(
-//           <div>xxx</div>
-//       )
-      
-//     }
-//   }
-
-
-function CurrencyTable(){
-    const [items, setItems] = useState([])
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        fetch("http://api.nbp.pl/api/exchangerates/rates/A/CHF/?format=json")
-        .then(res => res.json())
-        .then(
-          (result) => {
-            setItems(result.rates[0].mid)
-          },
-          error => {
-            setError(error);
-          }, []
-
-        
-        )
-    })
-    if(error){
-        return <div>{error.message}</div> 
-    } else {
-        return(
-            <div>{items}</div>
-        )
-        
-    }
-      
-
+  useEffect(() => {
+    fetch("http://api.nbp.pl/api/exchangerates/tables/a/?format=json")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setItems(result[0].rates);
+        },
+        (error) => {
+          setError(error);
+        }
+      );
+  }, []);
+  if (error) {
+    return <div>{error.message}</div>;
+  } else {
+    return (
+      <ul>
+        {items.map((item) => (
+          <li key={item.currency}>
+            {item.currency} {item.code} {item.mid}
+          </li>
+        ))}
+      </ul>
+    );
+  }
 }
-
 
 export default CurrencyTable;
