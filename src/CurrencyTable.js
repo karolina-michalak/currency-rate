fb78b74b6a56b623b234f32fa9757aacd0589c71
Karolina Michalak <karolina0michalak@gmail.com>
2020-11-15 15:09:29 +0100

    Removing element from favs

--- src/CurrencyTable.js
+++ src/CurrencyTable.js
@@ -1,53 +1,71 @@
-import React, { useEffect, useState, useRef} from "react";
+import React, { useEffect, useState, useRef } from "react";
 import axios from "axios";
 import Form from "./Form";
-import Modall from './Modal'
+import Modall from "./Modal";
 import { render } from "@testing-library/react";
 
 const CurrencyTable = () => {
-  const [items, setItems] = useState([]);
+  const [currencies, setCurrencies] = useState([]);
   const [error, setError] = useState(null);
-  const [favs, setFavs] = useState([])
-
+  const [favs, setFavs] = useState([]);
 
   useEffect(
     () => {
       axios
         .get("http://api.nbp.pl/api/exchangerates/tables/a/?format=json")
-        .then((result) => setItems(result.data[0].rates));
+        .then((result) => setCurrencies(result.data[0].rates));
     },
     (error) => {
       setError(error);
-    }, []
+    },
+    []
   );
 
   const addToFav = (item) => {
-    console.log('added to favs')
-    setItems(items.filter(el => el !== item))
-    setFavs([...favs, item])
-   
-    
-  }
+    setCurrencies(currencies.filter((el) => el !== item));
+    setFavs([...favs, item]);
+  };
 
-  const removeItem = () => {
-    
-  }
+  const removeFav = (item) => {
+    setFavs(favs.filter(el => el !== item))
+    setCurrencies([...currencies, item])
+  };
 
-  return(
+  return (
     <div>
       <h2>Ulubione:</h2>
-      {favs.length > 0 ? favs.map(item => (
-       <li key={item.code}> {item.currency} {item.code} {item.mid}  <button onClick={() => {removeItem()}}>x</button> </li>
-      )) : "nie ma ulubionych"}
+      {favs.length > 0
+        ? favs.map((item) => (
+            <li key={item.code}>
+              {" "}
+              {item.currency} {item.code} {item.mid}{" "}
+              <button
+                onClick={() => {
+                  removeFav(item);
+                }}
+              >
+                x
+              </button>{" "}
+            </li>
+          ))
+        : "nie ma ulubionych"}
       <h2>Lista walut:</h2>
       <ul>
-      {items.map(item => (
-        <li key={item.code}>{item.currency} {item.code} {item.mid} <button onClick={() => {addToFav(item)}}>fav</button></li>
-      ))}
+        {currencies.map((item) => (
+          <li key={item.code}>
+            {item.currency} {item.code} {item.mid}{" "}
+            <button
+              onClick={() => {
+                addToFav(item);
+              }}
+            >
+              fav
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
-  )
-
+  );
 };
 
 export default CurrencyTable;
